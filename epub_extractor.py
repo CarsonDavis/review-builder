1e20ae800f914bfecc901dfabd42199a63637c9b
Carson Davis <CarsonReidDavis@gmail.com>
2024-07-04 13:00:49 -0500

    streamline EpubExtractor class

--- epub_extractor.py
+++ epub_extractor.py
@@ -9,7 +9,12 @@ import os
 class EpubExtractor:
     def __init__(self, epub_file_path: str):
         self.epub_file_path = epub_file_path
+        self._validate_file_path()
         self.chapters = self._get_chapters()
+
+    def _validate_file_path(self) -> None:
+        if not os.path.exists(self.epub_file_path):
+            raise FileNotFoundError(f"File {self.epub_file_path} does not exist.")
 
     def _clean_text(self, text: str) -> str:
         text = re.sub(r"\n+", "\n", text)
@@ -35,13 +40,16 @@ class EpubExtractor:
             for chapter in chapters:
                 file.write(chapter + "\n\n")
 
-    def extract_and_save(self) -> str:
-        if not os.path.exists(self.epub_file_path):
-            raise FileNotFoundError(f"File {self.epub_file_path} does not exist.")
+    def _generate_output_path(self) -> str:
+        return os.path.splitext(self.epub_file_path)[0] + ".txt"
 
-        output_file = os.path.splitext(self.epub_file_path)[0] + ".txt"
-        self._write_to_txt(self.chapters, output_file)
-        return output_file
+    def extract(self) -> list[str]:
+        return self.chapters
+
+    def save(self, output_path=None) -> str:
+        output_path = output_path or self._generate_output_path()
+        self._write_to_txt(self.chapters, output_path)
+        return output_path
 
 
 if __name__ == "__main__":
@@ -53,7 +61,7 @@ if __name__ == "__main__":
 
     try:
         extractor = EpubExtractor(epub_file_path)
-        output_file = extractor.extract_and_save()
+        output_file = extractor.save()
         print(f"Chapters extracted and saved to {output_file}")
     except FileNotFoundError as e:
         print(e)
